eeaf56d0b65083e2dac8666dd03f4105792d4e13
Nicolas AYOUB <nicolas.ayoub5@outlook.com>
2021-06-11 18:49:37 +0200

    nouveau fichier base de donnee sql creation DAO avec methodes et BU

---
+++ Business/TerrainBU.cs
@@ -0,0 +1,23 @@
+﻿using Fr.EQL.Ai109.Totapatt.DataAccess;
+using Fr.EQL.Ai109.Totapatt.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fr.EQL.Ai109.Totapatt.Business
+{
+    public class TerrainBU
+    {
+        public Terrain GetTerrain(int id)
+        {
+            return new TerrainDAO().GetById(id);
+        }
+
+        public List<Terrain> GetByIdUtilisateur(int idUtilisateur)
+        {
+            return new TerrainDAO().GetByIdUtilisateur(idUtilisateur);
+        }
+    }
+}
