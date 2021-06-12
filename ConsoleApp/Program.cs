7ef39d43932c6561380df70a898fc991019969a0
Nicolas AYOUB <nicolas.ayoub5@outlook.com>
2021-06-12 16:54:13 +0200

    mise a jour classe TerrainDetails creation classe CompositionVegetationDetails renommage classe composition a CompositionVegetation mise a jour methode GetByIdWithDetails dans la classe terrainDAO

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,17 +8,21 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            //TerrainBU bu = new();
-            //Terrain t = bu.GetTerrain(264);
+            TerrainBU bu = new();
+            Terrain t = bu.GetTerrain(264);
 
             //Console.WriteLine("Nom terrain : " + t.NomTerrain + "\nAdresse : " + t.AdresseTerrain + "\nSurface : "
             //    + t.SurfaceTerrain + "\nDescription : " + t.DescriptionTerrain + "\nDate enregistrement : " + t.DateEnregistrementTerrain.ToString("dd/MM/yyyy"));
 
-            //TerrainDetails td = bu.GetByIdWithDetails(1);
-            //Console.WriteLine("Nom terrain : " + td.NomTerrain + "\nAdresse : " + td.AdresseTerrain + "\nSurface : "
-            //    + td.SurfaceTerrain + "\nDescription : " + td.DescriptionTerrain + "\nDate enregistrement : " + td.DateEnregistrementTerrain.ToString("dd/MM/yyyy")
-            //    + "\nNom proprieteaire : " + td.NomUtilisateur + "\nPrenom proprietaire : " + td.PrenomUtilisateur + "\nDescription proprietaire : " + td.DescriptionTerrain
-            //    + "\nNom ville : " + td.NomVilleTerrain + "\nCode postal : " + td.CodePostalTerrain);
+            TerrainDetails td = bu.GetByIdWithDetails(1);
+            Console.WriteLine("Nom terrain : " + td.NomTerrain + "\nAdresse : " + td.AdresseTerrain + "\nSurface : "
+                + td.SurfaceTerrain + "\nDescription : " + td.DescriptionTerrain + "\nDate enregistrement : " + td.DateEnregistrementTerrain.ToString("dd/MM/yyyy")
+                + "\nNom proprieteaire : " + td.NomUtilisateur + "\nPrenom proprietaire : " + td.PrenomUtilisateur + "\nDescription proprietaire : " + td.DescriptionTerrain
+                + "\nNom ville : " + td.NomVilleTerrain + "\nCode postal : " + td.CodePostalTerrain + "\nHumidite : " + td.HumiditeTerrain + "\nComposition : " + td.CompositionTerrain + "\nPente : " + td.PenteTerrain + "\nHauteur Herbe : " + td.HauteurHerbe);
+            foreach(CompositionVegetationDetails cds in td.CompositionsVegetationTerrain)
+            {
+                Console.WriteLine("Type vegetation : " + cds.TypeVegetaion + " --> Pourcentage : " + cds.PourcentageVegetation);
+            }
 
             //string mail = "Merci.McNully@gmail.com";
             //string mdp = "attilio";
