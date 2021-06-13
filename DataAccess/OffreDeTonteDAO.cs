2a0326ef476a5dfec34917e68550dd499565abc6
Nicolas AYOUB <nicolas.ayoub5@outlook.com>
2021-06-13 11:53:15 +0200

    correction OffreDeTonte

---
+++ DataAccess/OffreDeTonteDAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fr.EQL.Ai109.Tontapatt.DataAccess
+{
+    public class OffreDeTonteDAO
+    {
+
+    }
+}
---
+++ Model/OffreDeTonteDetails.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fr.EQL.Ai109.Tontapatt.Model
+{
+    public class OffreDeTonteDetails : OffreDeTonte
+    {
+        public string EspeceAnimal { get; set; }
+        public string RaceAnimal { get; set; }
+        public int MyProperty { get; set; }
+        public string NomUtilisateur { get; set; }
+        public string PrenomUtilisateur { get; set; }
+        public int DescriptionUtilisateur { get; set; }
+        public string NomVilleOfrreDeTonte { get; set; }
+        public string CodePostaleOffreDeTonte { get; set; }
+        public decimal LatitudeOffreDeTonte { get; set; }
+        public decimal LongitudeOffreDetonte { get; set; }
+        public OffreDeTonteDetails()
+        {
+        }
+
+        public OffreDeTonteDetails(OffreDeTonte offreDeTonte)
+        {
+            IdOffre = offreDeTonte.IdOffre;
+            IdRaceAnimal = offreDeTonte.IdRaceAnimal;
+            IdAnnulationOffre = offreDeTonte.IdAnnulationOffre;
+            IdUtilisateur = offreDeTonte.IdUtilisateur;
+            IdVilleCP = offreDeTonte.IdVilleCP;
+            NomOffre = offreDeTonte.NomOffre;
+            AdresseTroupeau = offreDeTonte.AdresseTroupeau;
+            DateDebutOffre = offreDeTonte.DateDebutOffre;
+            DateFinOffre = offreDeTonte.DateFinOffre;
+            DateCreationOffre = offreDeTonte.DateCreationOffre;
+            DescriptionOffre = offreDeTonte.DescriptionOffre;
+            DistanceMaximale = offreDeTonte.DistanceMaximale;
+            CoutAnimalParJour = offreDeTonte.CoutAnimalParJour;
+            DateAnnulationOffre = offreDeTonte.DateAnnulationOffre;
+            NombreAnimaux = offreDeTonte.NombreAnimaux;
+        }
+    }
+}
