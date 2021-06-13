ccf7fb44ee9e3b82c81e2cd89e5c5b35ac459e18
Paul Cuquémy <p.cuquemy@gmail.com>
2021-06-13 13:08:49 +0200

    Add new view connexion and controller

---
+++ WebApplication/Controllers/ConnexionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fr.EQL.Ai109.Totapatt.WebApplication.Controllers
+{
+    public class ConnexionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
