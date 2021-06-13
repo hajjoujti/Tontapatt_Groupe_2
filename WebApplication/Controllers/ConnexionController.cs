1dda2eb0d7e25e0e96691a3ff62430654716d444
Nicolas AYOUB <nicolas.ayoub5@outlook.com>
2021-06-13 22:48:27 +0200

    liaison methode connexion avec controller

--- WebApplication/Controllers/ConnexionController.cs
+++ WebApplication/Controllers/ConnexionController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fr.EQL.Ai109.Tontapatt.Business;
+using Fr.EQL.Ai109.Tontapatt.Model;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +10,30 @@ namespace Fr.EQL.Ai109.Totapatt.WebApplication.Controllers
 {
     public class ConnexionController : Controller
     {
+        Boolean isInBDD;
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Utilisateur utilisateur)
+        {
+            UtilisateurBU bu = new();
+            utilisateur = bu.GetUtilisateurAuthentification(utilisateur.MailUtilisateur, utilisateur.MdpUtilisateur);
+
+            if(utilisateur is null)
+            {
+                ViewBag.IsInBDD = false;
+                return View();
+            }
+            return View("/Home", utilisateur);
+        }
+
+        public IActionResult Echec()
+        {
+            
             return View();
         }
     }
