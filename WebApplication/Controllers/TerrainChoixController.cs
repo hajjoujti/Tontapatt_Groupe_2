91b46b6b41f0ef2c3e28722a553853bbb2eec76b
Paul Cuquémy <p.cuquemy@gmail.com>
2021-06-14 10:00:42 +0200

    add view terrainchoix

---
+++ WebApplication/Controllers/TerrainChoixController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fr.EQL.Ai109.Tontapatt.WebApplication.Controllers
+{
+    public class TerrainChoixController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
