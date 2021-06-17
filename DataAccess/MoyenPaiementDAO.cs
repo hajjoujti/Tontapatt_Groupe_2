b01bcaca190a5ff98ecc60c7505c08bc54b85651
Nicolas AYOUB <nicolas.ayoub5@outlook.com>
2021-06-17 11:26:25 +0200

    ajout methode GetAll moyen de paiement

---
+++ DataAccess/MoyenPaiementDAO.cs
@@ -0,0 +1,34 @@
+﻿using Fr.EQL.Ai109.Tontapatt.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fr.EQL.Ai109.Tontapatt.DataAccess
+{
+    public  class MoyenPaiementDAO : DAO
+    {
+        public List<MoyenPaiement> GetAll()
+        {
+            List<MoyenPaiement> moyensPaiement = new();
+            MySqlCommand cmd = CreerCommand();
+            cmd.CommandText = @"SELECT *
+                                FROM moyenpaiement";
+            cmd.Connection.Open();
+            MySqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                MoyenPaiement moyenPaiement = new();
+                moyenPaiement.IdMoyenPaiment = dr.GetInt32("id_moyen_paiement");
+                moyenPaiement.TypeMoyenPaiement = dr.GetString("type_moyen_paiement");
+                moyensPaiement.Add(moyenPaiement);
+            }
+
+            cmd.Connection.Close();
+            return moyensPaiement;
+        }
+    }
+}
