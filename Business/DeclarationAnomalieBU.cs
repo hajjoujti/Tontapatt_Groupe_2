7dbadb642d26282016e8257a84c061e47da8602a
Sorhaindo <marcsorhaindo@gmail.com>
2021-06-17 15:25:28 +0200

    anomalie back/front

---
+++ Business/DeclarationAnomalieBU.cs
@@ -0,0 +1,25 @@
+﻿using Fr.EQL.Ai109.Tontapatt.DataAccess;
+using Fr.EQL.Ai109.Tontapatt.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fr.EQL.Ai109.Tontapatt.Business
+{
+    public class DeclarationAnomalieBU
+    {
+        public void InsererDeclarationAnomalie(int idDemandeDeReservation, int idUtilisateurClient, int idUtilisateurEleveur, int idTypeAnomalie)
+        {
+            DateTime dateDeclenchementAnomalie = DateTime.Now;
+            DateTime dateFinAnomalie = new DateTime(dateDeclenchementAnomalie.Year, dateDeclenchementAnomalie.Month, dateDeclenchementAnomalie.Day, 9, 0, 0);
+            new DeclarationAnomalieDAO().InsererDeclarationAnomalie(idDemandeDeReservation, idUtilisateurClient, idUtilisateurEleveur, idTypeAnomalie, dateDeclenchementAnomalie, dateFinAnomalie);
+        }
+
+        public List<Anomalie> GetAllByIdDemandeDeReservastion(int idDemandeDeReservation)
+        {
+            return new DeclarationAnomalieDAO().GetAllByIdDemandeDeReservastion(idDemandeDeReservation);
+        }
+    }
+}
