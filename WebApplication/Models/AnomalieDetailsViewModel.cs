1bea569419bbcbc69c51981ac403566d5b644e97
Sorhaindo <marcsorhaindo@gmail.com>
2021-06-17 16:37:11 +0200

    anomalie controller en cours

---
+++ WebApplication/Models/AnomalieDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fr.EQL.Ai109.Tontapatt.WebApplication.Models
+{
+    public class AnomalieDetailsViewModel
+    {
+        public int IdAnomalie { get; set; }
+        public int IdDemande { get; set; }
+        public int IdUtilisateurClient { get; set; }
+        public int IdUtilisateurEleveur { get; set; }
+       [Required(ErrorMessage = "Vous devez choisir le type d'anomalie")]
+        public int IdTypeAnomalie { get; set; }
+       
+        public string? DescriptionAnomalie { get; set; }
+    }
+}
