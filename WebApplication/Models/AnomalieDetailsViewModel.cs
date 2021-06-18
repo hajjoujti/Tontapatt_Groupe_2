26da08bec25da014174e29221e0df908f51b3bc5
Nicolas AYOUB <nicolas.ayoub5@outlook.com>
2021-06-18 11:20:51 +0200

    fonctionnement de la daclaration anomalie pour les deux utilisateur.

--- WebApplication/Models/AnomalieDetailsViewModel.cs
+++ WebApplication/Models/AnomalieDetailsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fr.EQL.Ai109.Tontapatt.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,13 +9,20 @@ namespace Fr.EQL.Ai109.Tontapatt.WebApplication.Models
 {
     public class AnomalieDetailsViewModel
     {
-        public int IdAnomalie { get; set; }
         public int IdDemande { get; set; }
+
         public int IdUtilisateurClient { get; set; }
+
         public int IdUtilisateurEleveur { get; set; }
+
        [Required(ErrorMessage = "Vous devez choisir le type d'anomalie")]
         public int IdTypeAnomalie { get; set; }
        
+        [Required]
         public string? DescriptionAnomalie { get; set; }
+
+        public DemandeDeReservationDetails DemandeDeReservationDetails { get; set; }
+
+        public List<TypeAnomalie> TypesAnomalie { get; set; }
     }
 }
